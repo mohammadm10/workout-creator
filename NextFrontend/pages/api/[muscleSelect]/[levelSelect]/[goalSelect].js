c937c8263708d4a77432232d2c93261cb71c880c
mohammadm10 <mohammadali_10@hotmail.com>
2023-07-10 00:59:33 -0700

    commit

--- NextFrontend/pages/api/[muscleSelect]/[levelSelect]/[goalSelect].js
+++ NextFrontend/pages/api/[muscleSelect]/[levelSelect]/[goalSelect].js
@@ -1,44 +1,38 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
-  const { muscleSelect, levelSelect, goalSelect } = req.query;
-  const prompt = `Please provide me 4-6 exercises to do at the gym in a numbered format given the following information: 
-                The muscle I want to train is ${muscleSelect}, my gym experience is ${levelSelect}, and my fitness goal is ${goalSelect}.
-                Also provide me the number of sets and reps (without the '-' character, use the word 'to' (ex: 8 to 10 reps)) to do for each exercise based on my fitness goal and experience.
+    console.log("req.query", req.query)
+    const { muscleSelect, levelSelect, goalSelect } = req.query
+    const prompt = `Please provide me 4-6 exercises to do at the gym in a numbered format given the following information: 
+                The muscle I want to train is ${muscleSelect}, my gym experience is ${levelSelect} and my fitness goal is ${goalSelect}.
+                Also provide me the number of sets and reps (without the '-' character, use the word 'to' (ex: 8 to 10 reps)) to do for each exercise based off of my fitness goal and experience.
                 Number exercises as follows: 1. , 2. , etc. Briefly describe each workout and provide 1 tip for each one, in the form Tip: . For each workout, include
-                all text in a single paragraph. If you are using words like 'push-ups' with dashes, split them into two words 'push ups'`;
+                all text in a single paragraph. If you are using words like 'push-ups' with dashes, split them into to words 'push ups'`;
+    const model = 'gpt-3.5-turbo-0613';
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.NEXT_KEY}`,
+            },
+            body: JSON.stringify({
+                model,
+                messages: [{ role: 'system', content: prompt }],
+            }),
+        });
 
-  const model = 'gpt-3.5-turbo-0613';
-  const apiUrl = 'https://api.openai.com/v1/chat/completions';
-  const headers = {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_KEY}`,
-  };
-  const requestData = {
-    model,
-    messages: [{ role: 'system', content: prompt }],
-  };
+        const data = await response.json();
 
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(requestData),
-    });
-
-    const data = await response.json();
-    console.log('API Response:', data);
-
-    if (data.choices && data.choices.length > 0) {
-      const message = data.choices[0].message.content.trim();
-      console.log(message);
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json({ message });
-    } else {
-      res.status(500).json({ error: 'Unexpected response from OpenAI API' });
+        if (data.choices && data.choices.length > 0) {
+            const message = data.choices[0].message.content.trim();
+            console.log(message);
+            
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json({ message });
+        } else {
+            res.status(500).json({ error: 'Unexpected response from OpenAI API' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error ocurred: ' });
+        console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
 }
